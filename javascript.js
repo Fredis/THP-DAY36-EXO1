adc1797659ee88ca4dc99c5ce7018855b7563116
Fredis <fredericstojicevic@gmail.com>
2018-11-19 11:41:03 +0100

    Add the reverse function.

--- javascript.js
+++ javascript.js
@@ -13,6 +13,7 @@ function my_max(array)
 	alert("Nombre max : " + max)
 }
 
+
 function vowel_count(word)
 {
 
@@ -30,5 +31,19 @@ function vowel_count(word)
 	alert("Nombre de voyelles : " +  count_vowel);
 }
 
+
+function reverse(word)
+{
+	var count = word.length-1;
+	var reversed_string = "";
+
+	while(count != -1) {
+		reversed_string = reversed_string + word.charAt(count);
+		count -= 1;
+	}
+	alert("On inverse " + word + ", et ca donne : " + reversed_string);
+}
+
 my_max([10, 20, 5, 38, 17]);
 vowel_count("AaEEfghsO");
+reverse("Bonjour a tous !")
